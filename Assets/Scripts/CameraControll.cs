710b8fac6935aefe0d7007e6c522a80dd3069015
achu0810 <achu.blog0810@gmail.com>
2017-01-30 03:03:58 +0900

    first commit
    level3

---
+++ Assets/Scripts/CameraControll.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour {
+
+	public float speed;
+	Quaternion _attitude;
+
+	// Use this for initialization
+	void Start () {
+		#if UNITY_EDITOR
+		#elif UNITY_IOS || UNITY_ANDROID
+		Input.gyro.enabled = true;
+		#endif
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		#if UNITY_EDITOR
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			this.transform.Rotate (Vector3.up * -Time.deltaTime * speed, Space.World);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			this.transform.Rotate (Vector3.up * Time.deltaTime * speed, Space.World);
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			this.transform.Rotate (Vector3.left * Time.deltaTime * speed);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			this.transform.Rotate (Vector3.left * Time.deltaTime * -speed);
+		}
+
+		#elif UNITY_IOS || UNITY_ANDROID
+		_attitude = Input.gyro.attitude;
+		this.transform.rotation = Quaternion.AngleAxis(-90f, Vector3.right) * _attitude;
+		#endif
+	}
+}
